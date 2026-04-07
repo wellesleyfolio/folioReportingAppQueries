156d876c7a8427e99b3af2dff7cbf177ba8c15fc
Wellesley FOLIO Team <89404151+wellesleyfolio@users.noreply.github.com>
2026-04-07 12:06:56 -0400

    Update patrons_with_items_out.sql

--- queries/patrons_with_items_out.sql
+++ queries/patrons_with_items_out.sql
@@ -3,18 +3,15 @@ DROP FUNCTION IF EXISTS patrons_with_items_out;
 
 CREATE FUNCTION patrons_with_items_out()
 RETURNS TABLE
-	(patron_barcode text,
-    renewal_count integer,
+	(renewal_count integer,
     item_status text
 	)
 AS $$
 SELECT 	
-    users_u.barcode AS patron_barcode,
     circ_loan.renewal_count AS renewal_count,
     circ_loan.item_status AS item_status
 FROM
     folio_circulation.loan__t circ_loan
-    LEFT JOIN folio_users.users__t users_u ON (users_u.id = circ_loan.user_id)
 $$
 LANGUAGE SQL
 STABLE
