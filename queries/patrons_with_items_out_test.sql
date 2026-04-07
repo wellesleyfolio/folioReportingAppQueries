8821fbf805dc1b377f68c939ae74965648e14bbb
Wellesley FOLIO Team <89404151+wellesleyfolio@users.noreply.github.com>
2026-04-07 10:49:24 -0400

    Update patrons_with_items_out_test.sql

--- queries/patrons_with_items_out_test.sql
+++ queries/patrons_with_items_out_test.sql
@@ -9,7 +9,7 @@ RETURNS TABLE(
 AS $$
 SELECT 	
 	CONCAT('https://wellesley.folio.ebsco.com/users/preview/',users_u.id::uuid) AS "Link to Patron Record",
-	circ_loan_og.jsonb#>>'{status,name}' as loan_status,
+	jsonb_extract_path_text(circ_loan_og.jsonb, 'status', 'name') as loan_status, 
 	users_groups.group AS patron_group,
 	users_u.barcode AS patron_barcode
 FROM
@@ -17,7 +17,7 @@ FROM
 	LEFT JOIN folio_circulation.loan__ AS circ_loan_og ON circ_loan_og.id = circ_loan.id AND circ_loan_og.__current = TRUE
 	LEFT JOIN folio_users.users__t AS users_u ON users_u.id::uuid = circ_loan.user_id::uuid
 	LEFT JOIN folio_users.groups__t AS users_groups ON users_groups.id = users_u.patron_group
-WHERE circ_loan_og.jsonb#>>'{status,name}' = 'Open'
+WHERE jsonb_extract_path_text(circ_loan_og.jsonb, 'status', 'name') = 'Open'
 $$
 LANGUAGE SQL
 STABLE
