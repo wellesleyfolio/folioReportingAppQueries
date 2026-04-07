82660912f224b047bbaaec639a501282584eb42b
Wellesley FOLIO Team <89404151+wellesleyfolio@users.noreply.github.com>
2026-04-07 10:15:54 -0400

    Update patrons_with_items_out.sql

--- queries/patrons_with_items_out.sql
+++ queries/patrons_with_items_out.sql
@@ -3,20 +3,40 @@ DROP FUNCTION IF EXISTS get_users_items_out;
 
 CREATE FUNCTION get_users_items_out()
 RETURNS TABLE(
-  loan_status text,
-  patron_group text,
-  patron_barcode text,
-  last_name text,
-  first_name text
+	Link_to_patron text,
+	loan_status text,
+	patron_group text,
+	patron_barcode text,
+	first_name text,
+	preferred_first_name text,
+	middle_name text,
+	last_name text,	
+	email text,
+	affiliation_note text,
+    department_or_major text,
+    user_note text,
+    patron_expiration_date timestamptz,
+    item_location_at_checkout text,
+    title text, 
+    call_number text,
+    vol_chron_enum text,
+    item_barcode text,
+    renewal_count integer,
+    due_date timestamptz,
+    item_status text,
+    checked_out_date timestamptz,
+    integer,
+    patron_uuid uuid
 )
 AS $$
-/*WITH days AS (
+WITH days AS (
     SELECT 
         id,
         DATE_PART('day', NOW() - due_date) AS days_overdue
     FROM folio_circulation.loan__t 
-)*/
-SELECT 	--CONCAT('https://wellesley.folio.ebsco.com/users/preview/',users_u.id::uuid) AS "Link to Patron Record",
+)
+SELECT 	
+	CONCAT('https://wellesley.folio.ebsco.com/users/preview/',users_u.id::uuid) AS "Link to Patron Record",
 	circ_loan_og.jsonb#>>'{status,name}' as loan_status,
     users_groups.group AS patron_group,
     users_u.barcode AS patron_barcode,
@@ -27,7 +47,7 @@ SELECT 	--CONCAT('https://wellesley.folio.ebsco.com/users/preview/',users_u.id::
 	users_u_og.jsonb#>>'{personal,email}' AS email,
 	users_u_og.jsonb#>>'{customFields,affiliationNote}' AS affiliation_note,
     users_u_og.jsonb#>>'{customFields,departmentOrMajor}' AS department_or_major,
-    users_u_og.jsonb#>>'{customFields,userNote}' AS user_note/*,
+    users_u_og.jsonb#>>'{customFields,userNote}' AS user_note,
     to_char(users_u.expiration_date,'MM-DD-YYYY') AS patron_expiration_date,
     inv_loc.name AS item_location_at_checkout,
     inv_inst.index_title AS title,
@@ -42,7 +62,7 @@ SELECT 	--CONCAT('https://wellesley.folio.ebsco.com/users/preview/',users_u.id::
     circ_loan.item_status AS item_status,
     to_char(circ_loan.loan_date,'MM-DD-YYYY HH24:MM AM') AS checked_out_date,
     DATE_PART('day', NOW() - circ_loan.due_date) AS days_overdue,
-    users_u.id AS patron_uuid*/
+    users_u.id AS patron_uuid
 FROM
     folio_circulation.loan__t AS circ_loan --folio_reporting.loans_items AS li
     LEFT JOIN folio_circulation.loan__ AS circ_loan_og ON circ_loan_og.id = circ_loan.id AND circ_loan_og.__current = TRUE
@@ -53,7 +73,7 @@ FROM
     LEFT JOIN folio_inventory.holdings_record__t AS inv_hr ON inv_hr.id = inv_item.holdings_record_id
     LEFT JOIN folio_inventory.instance__t AS inv_inst ON inv_inst.id = inv_hr.instance_id
 	LEFT JOIN folio_inventory.location__t AS inv_loc ON inv_loc.id = circ_loan.item_effective_location_id_at_check_out
-    --LEFT JOIN days ON days.id = circ_loan.id
+    LEFT JOIN days ON days.id = circ_loan.id
 WHERE circ_loan_og.jsonb#>>'{status,name}' = 'Open'
 ORDER BY users_groups.group ASC, users_u_og.jsonb#>>'{personal,lastName}' ASC, users_u_og.jsonb#>>'{personal,firstName}' ASC, inv_item.effective_shelving_order ASC
   $$
